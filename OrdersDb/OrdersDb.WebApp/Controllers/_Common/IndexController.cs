b1f12936407591ce3bc655d76086171fd385a556
Andrei <andreialexandrovich@gmail.com>
2014-11-30 20:15:01 +0300

    development of order itrem editing form in process, created basic html and js models, spinner control refactoring

--- OrdersDb/OrdersDb.WebApp/Controllers/_Common/IndexController.cs
+++ OrdersDb/OrdersDb.WebApp/Controllers/_Common/IndexController.cs
@@ -1,12 +1,18 @@
 ﻿using System.Web.Mvc;
 using OrdersDb.Domain.Services._Common.Entities;
 
-namespace OrdersDb.WebApp.Controllers
+namespace OrdersDb.WebApp.Controllers._Common
 {
     public class IndexController : _Common.ControllerBase
     {
         [AccessType(AccessType.Update)]
         public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [AccessType(AccessType.Update)]
+        public ActionResult Test()
         {
             return View("Index");
         }
--- OrdersDb/OrdersDb.WebApp/Scripts/models/controls/modals/orderitemmodal.js
+++ OrdersDb/OrdersDb.WebApp/Scripts/models/controls/modals/orderitemmodal.js
@@ -1,10 +1,15 @@
 ﻿define([
         "knockout",
-        "./modalbase"
-], function (ko, modalbase) {
+        "./modalbase",
+         "Scripts/models/controls/fields/spinner",
+         "Scripts/models/controls/fields/dropdown"
+], function (ko, modalbase, spinner, dropdown) {
 
     function orderItemModal(parent) {
         var self = new modalbase(parent);
+        self.fields = {};
+        self.fields.product = new dropdown("Товар", 0, [], "Выберите товар из выпадающего списка");
+        self.fields.amount = new spinner("Количество", 1, 1, 1, 2000);
 
         return self;
     }
